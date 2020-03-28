1cf21059d5c320d6b9791e157384f661a58e6378
Gerry <Gerry@DESKTOP-27O1PE5.lan>
2020-03-28 16:27:55 +0100

    Completato test Hand

--- briscola/test/test/HandTest.java
+++ briscola/test/test/HandTest.java
@@ -45,7 +45,26 @@ public class HandTest {
 			assertTrue(copy.getCard(i).equals(hand.getCard(i)));
 		}
 		
-		}
+	}
+	
+	public void getCardTest() {
+		try {
+			hand=new Hand(3);
+			hand.add(Card.ASSO_BASTONI);
+			hand.add(Card.ASSO_COPPE);
+			hand.add(Card.ASSO_DENARI);
+		}catch(MaxCardNubmerExceededException exception) {};
+		
+		try {
+			Card prova=hand.getCard(1);
+			assertTrue(Card.ASSO_COPPE.equals(prova));
+			assertEquals(2, hand.getNumberCards());
+			prova=hand.getCard(1);
+			assertTrue(Card.ASSO_DENARI.equals(prova));
+			assertEquals(1, hand.getNumberCards());
+			assertThrows(ArrayIndexOutOfBoundsException.class, ()->{hand.getCard(1);});
+		}catch(ArrayIndexOutOfBoundsException exceptions) {};
+	}
 	
 	
 	
