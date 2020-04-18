c1188c5ce0cdef577d87728862ce54a4bb727878
carnivuth <62444605+carnivuth@users.noreply.github.com>
2020-04-18 17:50:11 +0200

    refactoring della classe hand per la gestione della mano tramite lista

--- briscola/test/test/DeckTest.java
+++ briscola/test/test/DeckTest.java
@@ -4,7 +4,8 @@ import deck.Deck;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class DeckTest {
     private static Deck d;
@@ -18,25 +19,21 @@ public class DeckTest {
     public void deckTest() {
 
         assertEquals(40, d.getCardNumber());
-        assertEquals(39, d.getLastCard());
-
     }
 
     @Test
     public void removeDueTest() {
+
         d.removeDue();
         assertEquals(39, d.getCardNumber());
-        assertEquals(38, d.getLastCard());
-
     }
 
     @Test
     public void getFirstCardTest() {
+
         d.getFirstCard();
         d.getFirstCard();
         assertEquals(38, d.getCardNumber());
-        assertEquals(37, d.getLastCard());
-
     }
 
 
